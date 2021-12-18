2f3153531d03d833b06e10917729c5648fef80d7
kyllsccs <kyllsi.ccs@gmail.com>
2021-12-18 13:25:41 +0800

    20211218_1.4 start to build Products Main -> details

--- pages/products.js
+++ pages/products.js
@@ -5,11 +5,11 @@ import ProductsCards from "../components/ProductsCards";
 import { ProductsContainer } from "../components/styles/Products.styled";
 
 // 迭代出卡片
-function products({ posts }) {
+function products({ main_posts }) {
     return (
         <ProductsContainer>
             <h1>Products</h1>
-            {posts.map((post, index) => (
+            {main_posts.map((post, index) => (
                 <ProductsCards key={index} post={post} />
             ))}
         </ProductsContainer>
@@ -22,37 +22,41 @@ export async function getStaticProps() {
     const details_folder_path =
         "Contents/products.posts/products.details.posts";
 
-
-    
-    const main_filesbox = fs.readdirSync(path.join(main_folder_path));
-    // console.log(files);
-    // 添加判斷為.md檔案的
-    const files = [];
-    for (let i = 0; i < main_filesbox.length; i++) {
-        if (main_filesbox[i].includes(".md")) {
-            files.push(main_filesbox[i]);
+    function exc_props(path_1) {
+        const main_filesbox = fs.readdirSync(path.join(path_1));
+        // console.log(files);
+        // 添加判斷為.md檔案的
+        const files = [];
+        for (let i = 0; i < main_filesbox.length; i++) {
+            if (main_filesbox[i].includes(".md")) {
+                files.push(main_filesbox[i]);
+            }
         }
+        console.log(files);
+
+        const posts = files.map((filename) => {
+            const slug = filename.replace(".md", "");
+            const markDownContents = fs.readFileSync(
+                path.join(path_1, filename),
+                "utf-8"
+            );
+
+            const { data: frontmatter } = matter(markDownContents);
+
+            return {
+                slug,
+                frontmatter,
+            };
+        });
+        return posts;
     }
-    console.log(files);
 
-    const posts = files.map((filename) => {
-        const slug = filename.replace(".md", "");
-        const markDownContents = fs.readFileSync(
-            path.join("Contents/products.posts", filename),
-            "utf-8"
-        );
-
-        const { data: frontmatter } = matter(markDownContents);
-
-        return {
-            slug,
-            frontmatter,
-        };
-    });
+    const main_posts = exc_props(main_folder_path);
+    const detail_posts = exc_props(details_folder_path);
 
     return {
         props: {
-            posts,
+            main_posts,
         },
     };
 }
