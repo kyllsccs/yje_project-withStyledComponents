b35f806f61f6f5600381aaf7fd18b7b1a13cbe2f
kyllsccs <kyllsi.ccs@gmail.com>
2021-12-19 16:33:07 +0800

    20211219_1.5.2 pre deploy to vps

--- components/ProductsCards.js
+++ components/ProductsCards.js
@@ -6,8 +6,8 @@ export default function PartnersCards({ main_post, detail_posts }) {
             <img src={main_post.frontmatter.cover_image} alt="" />
             <h3>{main_post.frontmatter.title}</h3>
             <p>{main_post.frontmatter.excerpt}</p>
-            {detail_posts.map((detail_post) => (
-                <button>{detail_post.frontmatter.title}</button>
+            {detail_posts.map((detail_post, index) => (
+                <button key={index}>{detail_post.frontmatter.title}</button>
             ))}
         </CardsContainer>
     );
