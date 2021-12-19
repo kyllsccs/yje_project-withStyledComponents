5366ffe3a013b8dcfa6e7085ec253decc85b82fc
kyllsccs <kyllsi.ccs@gmail.com>
2021-12-19 11:34:56 +0800

    20211219_1.1 single finished, start to mutli loop

--- pages/products.js
+++ pages/products.js
@@ -29,15 +29,26 @@ export async function getStaticProps() {
     // 1 先取得資料夾內的所有資料夾&&檔案名稱
     function path_array(enter_path) {
         const path_box = fs.readdirSync(path.join(enter_path));
-        return path_box;
+        const path_bank = [];
+        for (let i = 0; i < path_box.length; i++) {
+            let per_path = fs.readdirSync(path.join(enter_path, path_box[i]));
+            per_path.unshift(path_box[i]);
+            // console.log(path_box[i]);
+            path_bank.push(per_path);
+        }
+        // console.log(path_box);
+        console.log(path_bank);
+        // return 各個資料夾裡面的數據
+        return path_bank;
     }
 
     // 2 取出明細&&details
     function exc_props(path_1) {
-        const main_filesbox = path_array(path_1);
-        console.log(main_filesbox);
+        const main_path = `Contents/products.posts/${path_1}`;
+        const main_filesbox = fs.readdirSync(path.join(main_path));
+        // const main_filesbox = path_array(path_1);
+        // console.log(main_filesbox);
 
-        // console.log(files);
         // 添加判斷為.md檔案的
         const files = [];
         for (let i = 0; i < main_filesbox.length; i++) {
@@ -50,7 +61,7 @@ export async function getStaticProps() {
         const posts = files.map((filename) => {
             const slug = filename.replace(".md", "");
             const markDownContents = fs.readFileSync(
-                path.join(path_1, filename),
+                path.join(main_path, filename),
                 "utf-8"
             );
 
@@ -64,8 +75,17 @@ export async function getStaticProps() {
         return posts;
     }
 
-    const main_posts = exc_props(main_folder_path);
-    const detail_posts = exc_props(details_folder_path);
+    const path_banks = path_array(main_folder_path);
+    // console.log(path_banks);
+
+    const mainss = path_banks[0][0];
+    const details_s = `${mainss}/${path_banks[0][2]}`;
+
+    const main_posts = exc_props(mainss);
+    const detail_posts = exc_props(details_s);
+
+    // const main_posts = exc_props(main_folder_path);
+    // const detail_posts = exc_props(details_folder_path);
 
     return {
         props: {
